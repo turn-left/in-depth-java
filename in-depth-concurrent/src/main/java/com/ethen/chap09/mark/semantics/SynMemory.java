5066b2eea5ca30d06712576d6f1ba362ea8c9f71
ethenyang <ethenyang@126.com>
2021-08-03 23:58:50 +0800

    concurrency in action

---
+++ in-depth-concurrent/src/main/java/com/ethen/chap09/mark/semantics/SynMemory.java
@@ -0,0 +1,30 @@
+package com.ethen.chap09.mark.semantics;
+
+import com.ethen.common.SleepTools;
+
+/**
+ * 类说明：演示锁的内存语义
+ */
+public class SynMemory {
+    private static boolean ready;
+    private static int number;
+
+    private static class PrintThread extends Thread{
+        @Override
+        public void run() {
+            while(!ready){
+                System.out.println("number = "+number);
+            }
+            System.out.println("number = "+number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new PrintThread().start();
+        SleepTools.second(1);
+        number = 51;
+        ready = true;
+        SleepTools.second(5);
+        System.out.println("main is ended!");
+    }
+}
