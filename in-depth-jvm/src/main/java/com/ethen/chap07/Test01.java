82fe9f165dca216d5c659d8f228cb871b302295f
ethenyang <ethenyang@126.com>
2021-08-20 00:20:33 +0800

    rpc in action

---
+++ in-depth-jvm/src/main/java/com/ethen/chap07/Test01.java
@@ -0,0 +1,10 @@
+package com.ethen.chap07;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.err.println(1 << 0);
+        System.err.println(1 << 2);
+        System.err.println(1 << 3);
+        System.err.println(1 << 4);
+    }
+}
