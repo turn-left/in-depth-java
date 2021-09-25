04a3f285fd8bfdd226f119d6d642e5d4e7894789
ethenyang <ethenyang@126.com>
2021-09-25 18:54:30 +0800

    Spring AOP in action

--- in-depth-spring/spring-source-demo/src/main/java/com/ethen/aop/AopConfig.java
+++ in-depth-spring/spring-source-demo/src/main/java/com/ethen/aop/AopConfig.java
@@ -8,8 +8,11 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * 启用spring AOP
  * 注意需要能被component-scan扫描到！
  * <p>
- * Spring aop报错：com.sun.proxy.$Proxy xxx cannot be cast to xxx
+ * Spring AOP 报错：com.sun.proxy.$Proxy xxx cannot be cast to xxx 解决方案
  * {@link -> https://blog.csdn.net/zhouzhiwengang/article/details/50884353}
+ * <p>
+ * Spring AOP 方法嵌套问题解决方案
+ * {@link -> https://blog.csdn.net/hl_java/article/details/79445799}
  */
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @Configuration
