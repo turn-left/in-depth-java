f4737b9f05932a829f0d1165a276149213c72e00
ethenyang <ethenyang@126.com>
2021-10-17 21:50:03 +0800

    java agent update

---
+++ in-depth-agent/src/main/java/com/ethen/agent/AgentMain.java
@@ -0,0 +1,10 @@
+package com.ethen.agent;
+
+/**
+ * 主程序 => 待插桩程序
+ */
+public class AgentMain {
+    public static void main(String[] args) {
+        System.err.println("==================== AgentMain main方法执行 =====================");
+    }
+}
--- in-depth-agent/src/main/java/com/ethen/agent/MyAgent.java
+++ in-depth-agent/src/main/java/com/ethen/agent/MyAgent.java
@@ -2,8 +2,13 @@ package com.ethen.agent;
 
 import java.lang.instrument.Instrumentation;
 
+/**
+ * java agent 执行
+ * <p>
+ * fixme 待分析MANIFEST.MF文件定义
+ */
 public class MyAgent {
-    public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("=========== Hello, java agent, agentArgs=" + agentArgs + " ==========");
+    public static void premain(String args, Instrumentation inst) {
+        System.out.println("============= MyAgent 执行中 ===== args:" + args + " ============");
     }
 }
