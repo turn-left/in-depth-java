396d0ea5800057f7d8dc604d2c1d64fe6120976c
ethenyang <ethenyang@126.com>
2021-12-25 17:05:15 +0800

    alipay init

---
+++ in-depth-alipay/f2f-pay-app/src/main/java/com/ethen/alipay/F2fPayAppApplication.java
@@ -0,0 +1,13 @@
+package com.ethen.alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F2fPayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F2fPayAppApplication.class, args);
+	}
+
+}
